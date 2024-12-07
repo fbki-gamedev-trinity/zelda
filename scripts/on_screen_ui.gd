ba95daccf97f9c19cad5aaef1009cc1fb07ee086
Alyona1619 <alena.g190503@gmail.com>
2024-12-07 14:17:54 +0800

    add health bar for player and dead animation

--- scripts/on_screen_ui.gd
+++ scripts/on_screen_ui.gd
@@ -7,6 +7,8 @@ class_name OnScreenUI
 @onready var potion_slot: OnScreenEquipmentSlot = %PotionSlot
 @onready var spell_slot: OnScreenEquipmentSlot = %SpellSlot
 
+@onready var progress_bar: ProgressBar = $MarginContainer/ProgressBar
+
 @onready var slots_dictionary = {
 	"Right_Hand": right_hand_slot,
 	"Left_Hand": left_hand_slot,
@@ -15,3 +17,9 @@ class_name OnScreenUI
 
 func equip_item(item: InventoryItem, slot_to_equip: String):
 	slots_dictionary[slot_to_equip].set_equipment_texture(item.texture)
+
+func init_health_bar(max_health: int) -> void:
+	progress_bar.max_value = max_health
+	
+func apply_damage_to_health_bar(damage: int):
+	progress_bar.value -= damage
